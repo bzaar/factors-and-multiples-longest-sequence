19c2f822393d11bb5fb8207d8bd01a2f61501f9f
Sergey Slepov <slepov@gmail.com>
2022-05-15 18:43:28 +0100

    Use file-scoped namespaces

--- FactorsAndMultiples.UnitTests.cs
+++ FactorsAndMultiples.UnitTests.cs
@@ -1,19 +1,18 @@
 using NUnit.Framework;
 
-namespace FactorsAndMultiplesGame
+namespace FactorsAndMultiplesGame;
+
+public class FactorsAndMultiplesUnitTests
 {
-    public class FactorsAndMultiplesUnitTests
+    [Test]
+    [TestCase(44, "88 1 2 4 11 22")]
+    [TestCase(55, "1 5 11")]
+    [TestCase(50, "100 1 2 5 10 25")]
+    public void Test1(int n, string expected)
     {
-        [Test]
-        [TestCase(44, "88 1 2 4 11 22")]
-        [TestCase(55, "1 5 11")]
-        [TestCase(50, "100 1 2 5 10 25")]
-        public void Test1(int n, string expected)
-        {
-            string actual = string.Join(" ", _factorsAndMultiples.Get(n));
-            Assert.AreEqual(expected, actual);
-        }
-        
-        private readonly FactorsAndMultiples _factorsAndMultiples = new (100);
+        string actual = string.Join(" ", _factorsAndMultiples.Get(n));
+        Assert.AreEqual(expected, actual);
     }
+    
+    private readonly FactorsAndMultiples _factorsAndMultiples = new (100);
 }
--- FactorsAndMultiples.cs
+++ FactorsAndMultiples.cs
@@ -1,22 +1,19 @@
-﻿using System.Collections.Generic;
+﻿namespace FactorsAndMultiplesGame;
 
-namespace FactorsAndMultiplesGame
+public record FactorsAndMultiples(int Max)
 {
-    public record FactorsAndMultiples(int Max)
+    public IEnumerable<int> Get(int num)
     {
-        public IEnumerable<int> Get(int num)
+        for (int m = num * 2; m <= Max; m += num)
         {
-            for (int m = num * 2; m <= Max; m += num)
+            yield return m;
+        }
+    
+        for (int f = 1; f <= num / 2; ++f)
+        {
+            if (num % f == 0)
             {
-                yield return m;
-            }
-        
-            for (int f = 1; f <= num / 2; ++f)
-            {
-                if (num % f == 0)
-                {
-                    yield return f;
-                }
+                yield return f;
             }
         }
     }
