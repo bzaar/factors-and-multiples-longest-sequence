3e7e9fb9da1ef5efe5d33168b784078509a519df
Sergey Slepov <slepov@gmail.com>
2022-05-15 15:01:08 +0100

    Add test for 50

--- FactorsAndMultiples.UnitTests.cs
+++ FactorsAndMultiples.UnitTests.cs
@@ -7,6 +7,7 @@ namespace FactorsAndMultiplesGame
         [Test]
         [TestCase(44, "88 1 2 4 11 22")]
         [TestCase(55, "1 5 11")]
+        [TestCase(50, "100 1 2 5 10 25")]
         public void Test1(int n, string expected)
         {
             string actual = string.Join(" ", _factorsAndMultiples.Get(n));
